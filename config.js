2ddb48d02b13c8924011f36d990bec1a451ae5d9
Urukagall <dany.jorge@free.fr>
2023-12-05 14:01:38 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+export const publicToken = "public_ZqC8xOv6l9P4-h8f";
+export const mainSceneUUID = "36e561f8-fdb3-4ada-a556-89fd40f5fe97";
+export const characterControllerSceneUUID = "6ec01c5d-9b06-4748-ad56-20598a932c7c";
