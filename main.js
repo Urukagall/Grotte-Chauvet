9fe2a428cdc48697393c641c98874fd66331ee08
Urukagall <dany.jorge@free.fr>
2023-12-06 16:09:55 +0100

    affichage text

--- main.js
+++ main.js
@@ -108,12 +108,12 @@ async function checkKeyPressed(event){
 
 async function changeStateTorch(){
   const torch = await SDK3DVerse.engineAPI.findEntitiesByEUID('bcc769ca-6cec-4c89-a8d7-bd408a3f4142');
-  if(torch[0].components.point_light.intensity == 20) {
+  if(torch[0].components.point_light.intensity == 5) {
     torch[0].setComponent('point_light', { intensity: 0 });
   }
   
   else {
-    torch[0].setComponent('point_light', { intensity: 20 });
+    torch[0].setComponent('point_light', { intensity: 5 });
   }
   
 }
@@ -123,15 +123,27 @@ async function detection(){
   const rect   = canvas.getClientRects()[0];
   var { entity, pickedPosition, pickedNormal } = await SDK3DVerse.engineAPI.castScreenSpaceRay(rect.x + rect.width / 2, rect.y + rect.height / 2, true, false);
   const fresque = await SDK3DVerse.engineAPI.findEntitiesByEUID('3326febf-4b07-4e96-be3e-cf5e80b368fa');
-  const user = await SDK3DVerse.engineAPI.findEntitiesByEUID(characterControllerSceneUUID);
-  //console.log(fresque[0].getGlobalTransform().position);
-  const posFresque = fresque[0].getGlobalTransform().position;
-if(entity){
-  const fresqueFront = entity.getAncestors();
-  if(fresque[0].getEUID() == fresqueFront[0].components.euid.value){
-    console.log("dani TG");
+  const user = await SDK3DVerse.engineAPI.cameraAPI.getActiveViewports();
+
+  const childrenFresque = await fresque[0].getChildren();
+  const posFresque = await childrenFresque[0].getGlobalTransform().position;
+  const posUser = await user[0].getTransform().position;
+  const dist = Math.sqrt( ((posFresque[0] - posUser[0]) **2 ) + ((posFresque[1] - posUser[1]) **2) + ((posFresque[2] - posUser[2]) **2));
+  console.log(posFresque);
+  console.log(posUser);
+  console.log(dist);
+  if(entity && dist<7){
+    const fresqueFront = entity.getAncestors();
+  
+    if(fresque[0].getEUID() == fresqueFront[0].components.euid.value){
+
+      childrenFresque[1].setVisibility(!childrenFresque[1].isVisible());
+
+    }
   }
-}
+  else if (dist >= 7){
+    childrenFresque[1].setVisibility(false);
+  }
 
   // const {entity, pickedPosition, pickedNormal} = await SDK3DVerse.engineAPI.castScreenSpaceRay(e.clientX, e.clientY, selectEntity, keepOldSelection);
   // entity ? console.log('Selected entity', entity.getName()) : console.log('No entity selected');
